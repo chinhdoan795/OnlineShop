7a5b18b27e0f8262809794f4fe12c0bcd45f3a23
chinhdoan795 <52903177+chinhdoan795@users.noreply.github.com>
2020-06-18 09:24:25 +0700

    Update product.js

--- product.js
+++ product.js
@@ -1,6 +1,18 @@
 const express = require('express');
 var router = express.Router();
 
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb+srv://Test:12345@cluster0-9snej.mongodb.net/test';
+
+
+router.get('/',async (req,res)=>{
+    let client= await MongoClient.connect(url);
+    let dbo = client.db("MyDb");
+   
+    let results = await dbo.collection("products").find({}).toArray();
+    res.render('allProducts',{customers:results});
+})
+
 router.get('/add',(req,res)=>{
     res.render('addproduct');
 })
